43643d74a3ac35c1859cf992ebb2c78074122299
さとうまこと <satomakoto@protonmail.com>
2020-10-09 18:06:26 +0900

    edit tag sequence

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
 * hexo-tag-blog-card
 *
-* Copyright (c) 2019, hinastory
+* Copyright (c) 2020, hinastory, sat0ma014
 * Licensed under the MIT license.
 * Syntax:
 * {% blogCard <url> [rel:<rel> [target:<target>] [useHatena:<true/false>] %}
@@ -13,15 +13,15 @@ const ogs = require('open-graph-scraper');
 const escapeHTML = require('escape-html');
 const url = require('url');
 const descriptionLength = (hexo.config.blogCard && hexo.config.blogCard.descriptionLength)
-                            ? hexo.config.blogCard.descriptionLength : 140;
+  ? hexo.config.blogCard.descriptionLength : 140;
 const className = (hexo.config.blogCard && hexo.config.blogCard.className)
-                    ? hexo.config.blogCard.className : 'blog-card';
+  ? hexo.config.blogCard.className : 'blog-card';
 const faviconAPI = (hexo.config.blogCard && hexo.config.blogCard.faviconAPI)
-                    ? hexo.config.blogCard.faviconAPI : 'http://favicon.hatena.ne.jp/?url=$URL';
+  ? hexo.config.blogCard.faviconAPI : 'http://favicon.hatena.ne.jp/?url=$URL';
 const useHatena = (hexo.config.blogCard && hexo.config.blogCard.useHatena)
-                    ? hexo.config.blogCard.useHatena : false;
+  ? hexo.config.blogCard.useHatena : false;
 const timeout = (hexo.config.blogCard && hexo.config.blogCard.timeout)
-                    ? hexo.config.blogCard.timeout : 4000;
+  ? hexo.config.blogCard.timeout : 4000;
 
 hexo.extend.tag.register('blogCard', function(args) {
   return getTag(parseOption(args));
@@ -79,7 +79,7 @@ function getTagByOpenGraph(options){
     .catch(function (error) {
       console.log('error:', error);
       return '';
-  });
+    });
 }
 
 function getInfo(options, ogp) {
@@ -104,19 +104,19 @@ function getContents(options, ogp) {
   let contents = '';
   let text = '';
 
-  if (ogp.hasOwnProperty('ogImage')) {
-    const image = util.htmlTag('img', { src: ogp.ogImage.url } , '');
-    contents = util.htmlTag('div', { class: 'hbc-thumbnail' }, image);
-  }
-
   text += util.htmlTag('div', { class: 'hbc-title' }, escapeHTML(ogp.ogTitle));
   text += util.htmlTag('div', { class: 'hbc-url' }, options.url);
 
   if (ogp.hasOwnProperty('ogDescription')) {
     const description = adjustLength(ogp.ogDescription);
     text += util.htmlTag('div', { class: 'hbc-description' }, escapeHTML(description));
+    contents = util.htmlTag('div', { class: 'hbc-text' }, text);
   }
-  contents += util.htmlTag('div', { class: 'hbc-text' }, text);
+
+  if (ogp.hasOwnProperty('ogImage')) {
+    const image = util.htmlTag('img', { src: ogp.ogImage.url } , '');
+    contents += util.htmlTag('div', { class: 'hbc-thumbnail' }, image);
+  }
 
   return util.htmlTag('div', { class: 'hbc-contents' },  contents);
 }
